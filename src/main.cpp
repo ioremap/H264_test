5d6e429978056779b40356736e6e24c64536980c
ioremap <32408895+ioremap@users.noreply.github.com>
2020-11-03 17:06:55 +0800

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+
+#include "stdio.h"
+#include "stdint.h"
+#include "x264.h"
+#include "x264_config.h"
+
+
+int main()
+{
+	x264_param_t param;
+	x264_param_default(&param);
+	
+	printf("==================================\n");
+
+	return 0;
+}
